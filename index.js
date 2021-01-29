319138085773aa369b5392a142c2c1bc7f1f849c
nmhummel <natalie.hummel@gmail.com>
2021-01-29 11:14:17 -0600

    commit tree c63a1db23e9a92f300eb7ebe8c0e83e6db75c6c6

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+/* Write your implementation of displayMessage() */
+
+function displayMessage(stringArg) {
+  document.getElementById("greeting").innerText = stringArg;
+}
 
 /* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(timeString) {
+  const time = parseInt(timeString, 10);
+  if (time < 12) {
+    return "Good Morning"
+  } 
+  else if (time > 17) {
+    return "Good Evening"
+  } 
+  else {
+    return "Good Afternoon"
+  }
+}
